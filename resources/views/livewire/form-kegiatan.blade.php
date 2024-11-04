2a047e64a869f688fbcb196317bd546796e9c2ae
Vendy Alifian <vanselite21@gmail.com>
2024-11-04 22:31:15 +0700

    fix form-kegiatan content

--- resources/views/livewire/form-kegiatan.blade.php
+++ resources/views/livewire/form-kegiatan.blade.php
@@ -6,7 +6,6 @@ use App\Models\Dusun;
 use Livewire\Volt\Component;
 
 new class extends Component {
-
     public $statuses = [
         [
             'id' => 1,
@@ -90,25 +89,86 @@ new class extends Component {
         // Filter data berdasarkan bidang yang dipilih
         $this->programs = Program::where('id_bidang', $bidangId)->get();
     }
-
-
-
-
 }; ?>
 
 <div>
     <x-form wire:submit.prevent="handleSubmit">
-        <x-select label="Bidang" :options="$bidangs" option-value="id" option-label="nama" wire:model.live="selectedBidang" />
-        <x-select label="Program" :options="$programs" option-value="id" option-label="nama" wire:model="selectedUser" />
-        <x-input label="Nama Kegiatan" wire:model="name" />
-        <x-select label="Status" :options="$statuses" option-value="value" option-label="nama" wire:model="selectedUser" />
-        <x-input label="Volume" wire:model="name" />
-        <x-select label="Satuan" :options="$satuans" option-value="value" option-label="nama" wire:model="selectedUser" />
-        <x-input label="Lokasi" wire:model="name" />
-        <x-select label="Dusun" icon="o-user" :options="$dusuns" option-value="id" option-label="nama" wire:model="selectedUser" />
-        <x-input label="Longitude" wire:model="name" />
-        <x-input label="Latitude" wire:model="name" />
+        <x-select label="Bidang" :options="$bidangs" option-value="id" option-label="nama"
+            wire:model.live="selectedBidang" />
+        <div class="grid grid-cols-10 gap-4 h-30">
+            <div class="col-span-8">
+                <x-select label="Program" :options="$programs" option-value="id" option-label="nama"
+                wire:model="selectedUser" />
+            </div>
+            <div class="justify-center col-span-2 mx-5 pt-7 ">
+                <x-button label="Tambah Program" icon="o-home" class="btn-outline" responsive />
+            </div>
+        </div>
+        <div class="grid gap-4 md:grid-cols-2">
+            <div>
+                <x-input label="Nama Kegiatan" wire:model="name" />
+            </div>
+            <div>
+                <x-select label="Status" :options="$statuses" option-value="value" option-label="nama"
+                    wire:model="selectedUser" />
+            </div>
+        </div>
+
+        <div class="grid gap-4 md:grid-cols-2">
+            <div>
+                <x-input label="Volume" wire:model="name" />
+            </div>
+            <div>
+                <x-select label="Satuan" :options="$satuans" option-value="value" option-label="nama"
+                    wire:model="selectedUser" />
+            </div>
+        </div>
+
+        <div class="grid gap-16 my-2 md:grid-cols-6">
+            <div>
+                <x-checkbox label="Tahun 1" wire:model="tahun_1" />
+            </div>
+            <div>
+                <x-checkbox label="Tahun 2" wire:model="tahun_2" />
+            </div>
+            <div>
+                <x-checkbox label="Tahun 3" wire:model="tahun_3" />
+            </div>
+            <div>
+                <x-checkbox label="Tahun 4" wire:model="tahun_4" />
+            </div>
+            <div>
+                <x-checkbox label="Tahun 5" wire:model="tahun_5" />
+            </div>
+            <div>
+                <x-checkbox label="Tahun 6" wire:model="tahun_6" />
+            </div>
+        </div>
+
+        <div class="grid gap-4 md:grid-cols-2">
+            <div>
+                <x-input label="Lokasi" wire:model="name" />
+            </div>
+            <div>
+                <x-select label="Dusun" :options="$dusuns" option-value="id" option-label="nama"
+                    wire:model="selectedUser" />
+            </div>
+        </div>
+
         <x-menu-separator />
+        <div class="grid gap-4 md:grid-cols-2">
+            <div>
+                <x-input label="Longitude" wire:model="name" />
+            </div>
+            <div>
+                <x-input label="Latitude" wire:model="name" />
+            </div>
+            <div class="col-span-2">
+                <x-textarea label="Mapbox" />
+            </div>
+        </div>
+        <hr />
+
         <x-textarea label="Deskripsi" wire:model="bio" placeholder="Your story ..." hint="Max 1000 chars" rows="5"
             inline />
 
@@ -120,10 +180,10 @@ new class extends Component {
 </div>
 
 @script
-    <script>
-        function handleSubmit() {
+<script>
+    function handleSubmit() {
             // Lakukan sesuatu di sini, misalnya menampilkan alert
             alert('Form submitted!');
         }
-    </script>
+</script>
 @endscript
