f30eba7422d51dbc1ca3b3c7ac841c2c69f1ce88
Vendy Alifian <vanselite21@gmail.com>
2024-11-05 20:49:07 +0700

    update formKegiatan with almost completed backend

--- resources/views/livewire/form-kegiatan.blade.php
+++ resources/views/livewire/form-kegiatan.blade.php
@@ -3,6 +3,8 @@
 use App\Models\Bidang;
 use App\Models\Program;
 use App\Models\Dusun;
+use App\Models\Kegiatan;
+use Livewire\Attributes\Validate;
 use Livewire\Volt\Component;
 
 new class extends Component {
@@ -64,14 +66,46 @@ new class extends Component {
         ],
     ];
 
+    //init data dari db
     public $bidangs;
-
-    public $selectedBidang;
-
     public $programs;
-
     public $dusuns;
 
+    //init input data
+   #[Validate('required', message: 'Tolong Pilih Salah Satu Bidang')]
+    public $selectedBidang;
+    #[Validate('required', message: 'Tolong Pilih Salah Satu Program')]
+    public $selectedProgram;
+    #[Validate('required', message: 'Jangan Kosongkan Nama')]
+    public $nama;
+    #[Validate('required', message: 'Tolong Pilih Salah Satu Status')]
+    public $selectedStatus;
+    #[Validate('required', message: 'Jangan Kosongan Volume')]
+    public $volume;
+    #[Validate('required', message: 'Tolong Pilih Salah Satu Status Kegiatan')]
+    public $selectedSatuan;
+
+    public $tahun_1 = false;
+    public $tahun_2 = false;
+    public $tahun_3 = false;
+    public $tahun_4 = false;
+    public $tahun_5 = false;
+    public $tahun_6 = false;
+
+    #[Validate('required', message: 'Jangan Kosongkan Lokasi Kegiatan')]
+    public $lokasi;
+    #[Validate('required', message: 'Tolong Pilih Salah Satu Dusun Kegiatan')]
+    public $selectedDusun;
+    #[Validate('required', message: 'Tolong Pilih Lokasi pada Map')]
+    public $longitude;
+    #[Validate('required', message: 'Tolong Pilih Lokasi pada Map')]
+    public $latitude;
+    public $progres = 0;
+    #[Validate('required', message: 'Jangan Kosongkan Deksripsi Kegiatan')]
+    public $deskripsi;
+
+
+    //Inisialisasi $ yang diperlukan dalam logic
     public function mount()
     {
         $this->selectedBidang = null;
@@ -83,22 +117,49 @@ new class extends Component {
 
         $this->dusuns = Dusun::all();
     }
-
+    //Update Selected Bidang
     public function updatedSelectedBidang($bidangId)
     {
         // Filter data berdasarkan bidang yang dipilih
         $this->programs = Program::where('id_bidang', $bidangId)->get();
     }
+
+    public function store()
+    {
+
+        $validated = $this->validate();
+
+        Kegiatan::create([
+            'nama' => $this->nama,
+            'id_program' => $this->selectedProgram,
+            'status' => $this->selectedStatus,
+            'volume' => $this->nama,
+            'satuan' => $this->selectedSatuan,
+            'tahun_1' => $this->tahun_1,
+            'tahun_2' => $this->tahun_2,
+            'tahun_3' => $this->tahun_3,
+            'tahun_4' => $this->tahun_4,
+            'tahun_5' => $this->tahun_5,
+            'tahun_6' => $this->tahun_6,
+            'lokasi' => $this->lokasi,
+            'id_dusun' => $this->selectedDusun,
+            'longitude' => $this->longitude,
+            'latitude' => $this->latitude,
+            'progres' => $this->progres,
+            'deskripsi' => $this->deskripsi,
+    ]);
+
+    session()->flash('message', 'Data berhasil disimpan.');
+    }
 }; ?>
 
 <div>
-    <x-form wire:submit.prevent="handleSubmit">
-        <x-select label="Bidang" :options="$bidangs" option-value="id" option-label="nama"
-            wire:model.live="selectedBidang" />
+    <x-form wire:submit.prevent="store">
+        <x-select label="Bidang" :options="$bidangs" option-value="id" option-label="nama" wire:model.live="selectedBidang" />
         <div class="grid grid-cols-10 gap-4 h-30">
             <div class="col-span-8">
                 <x-select label="Program" :options="$programs" option-value="id" option-label="nama"
-                wire:model="selectedUser" />
+                    wire:model="selectedProgram" />
             </div>
             <div class="justify-center col-span-2 mx-5 pt-7 ">
                 <x-button label="Tambah Program" icon="o-home" class="btn-outline" responsive />
@@ -106,21 +167,21 @@ new class extends Component {
         </div>
         <div class="grid gap-4 md:grid-cols-2">
             <div>
-                <x-input label="Nama Kegiatan" wire:model="name" />
+                <x-input label="Nama Kegiatan" wire:model="nama" />
             </div>
             <div>
                 <x-select label="Status" :options="$statuses" option-value="value" option-label="nama"
-                    wire:model="selectedUser" />
+                    wire:model="selectedStatus" />
             </div>
         </div>
 
         <div class="grid gap-4 md:grid-cols-2">
             <div>
-                <x-input label="Volume" wire:model="name" />
+                <x-input label="Volume" wire:model="volume" />
             </div>
             <div>
                 <x-select label="Satuan" :options="$satuans" option-value="value" option-label="nama"
-                    wire:model="selectedUser" />
+                    wire:model="selectedSatuan" />
             </div>
         </div>
 
@@ -147,21 +208,21 @@ new class extends Component {
 
         <div class="grid gap-4 md:grid-cols-2">
             <div>
-                <x-input label="Lokasi" wire:model="name" />
+                <x-input label="Lokasi" wire:model="lokasi" />
             </div>
             <div>
                 <x-select label="Dusun" :options="$dusuns" option-value="id" option-label="nama"
-                    wire:model="selectedUser" />
+                    wire:model="selectedDusun" />
             </div>
         </div>
 
         <x-menu-separator />
         <div class="grid gap-4 md:grid-cols-2">
             <div>
-                <x-input label="Longitude" wire:model="name" />
+                <x-input label="Longitude" wire:model="longitude" />
             </div>
             <div>
-                <x-input label="Latitude" wire:model="name" />
+                <x-input label="Latitude" wire:model="latitude" />
             </div>
             <div class="col-span-2">
                 <x-textarea label="Mapbox" />
@@ -169,7 +230,7 @@ new class extends Component {
         </div>
         <hr />
 
-        <x-textarea label="Deskripsi" wire:model="bio" placeholder="Your story ..." hint="Max 1000 chars" rows="5"
+        <x-textarea label="Deskripsi" wire:model="deskripsi" placeholder="Tuliskan Deskripsi Kegiatan ..." hint="Max 1000 chars" rows="5"
             inline />
 
         <x-slot:actions>
