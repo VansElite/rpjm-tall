aa5cbb5cdb725c1c92c9094e3e196843bb160b28
Vendy Alifian <vanselite21@gmail.com>
2024-11-09 16:59:45 +0700

    add "add data" button

--- resources/views/livewire/navbar.blade.php
+++ resources/views/livewire/navbar.blade.php
@@ -38,6 +38,17 @@ new class extends Component {
 
     <div id="space" class="flex-grow"></div>
 
+    <details class="dropdown">
+        <summary class="btn btn-ghost m-1">
+            <x-icon name="o-plus" />
+            <span>Tambah</span>
+        </summary>
+        <ul class="menu dropdown-content bg-base-100 rounded-box z-[1] w-52 p-2 shadow">
+            <li><a wire:navigate href="/form">Program</a></li>
+            <li><a wire:navigate href="/form">Kegiatan</a></li>
+        </ul>
+    </details>
+
     <x-button label="Dashboard" icon="s-book-open" link="###" class="btn-ghost" responsive />
 
     <x-button label="User Management" icon="o-user-group" link="###" class="btn-ghost" responsive />
