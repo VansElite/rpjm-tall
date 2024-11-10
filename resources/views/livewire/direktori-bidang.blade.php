5b62be4cb684b1e45a58f2ad20f8dfcc2f755839
Vendy Alifian <vanselite21@gmail.com>
2024-11-10 18:21:39 +0700

    add delete bidang

--- resources/views/livewire/direktori-bidang.blade.php
+++ resources/views/livewire/direktori-bidang.blade.php
@@ -10,14 +10,23 @@ state([
 
 mount(function () {
     $this->headers = [
-        ['key' => 'id', 'label' => 'Nomor'],
+        ['key' => 'id', 'label' => 'ID'],
         ['key' => 'nama', 'label' => 'Nama Bidang'],
     ];
     $this->data = Bidang::all();
-})
+});
+
+$delete = function ($id) {
+    Bidang::destroy($id);
+    return to_route('direktori-bidang');
+}
 
 ?>
 
 <div class="m-4">
-    <x-table :headers="$headers" :rows="$data" striped />
+    <x-table :headers="$headers" :rows="$data">
+        @scope('actions', $row_bidang)
+            <x-button icon="o-trash" wire:click="delete({{ $row_bidang->id }})" spinner class="btn-sm" />
+        @endscope
+    </x-table>
 </div>
