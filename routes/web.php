e821f1a95823a934c250a556ba9325c4b21d023e
Vendy Alifian <vanselite21@gmail.com>
2024-11-12 17:08:41 +0700

    add route for edit

--- routes/web.php
+++ routes/web.php
@@ -10,15 +10,19 @@ Volt::route('/', 'index');
 
 Volt::route('/bidang','direktori-bidang')->name('direktori-bidang');
 Volt::route('/bidang/add','form-bidang')->name('add-bidang');
+Volt::route('/bidang/{bidang}/edit','form-edit-bidang')->name('edit-bidang');
 
 Volt::route('/program','direktori-program')->name('direktori-program');
 Volt::route('/program/add','form-program')->name('add-program');
+Volt::route('/program/{program}/edit','form-edit-program')->name('edit-program');
 
 Volt::route('/kegiatan','direktori-kegiatan')->name('direktori-kegiatan');
 Volt::route('/kegiatan/add','form-kegiatan')->name('add-kegiatan');
+Volt::route('/kegiatan/{kegiatan}/edit','form-edit-kegiatan')->name('edit-kegiatan');
 
 Volt::route('/laporan','direktori-laporan')->name('direktori-laporan');
 Volt::route('/laporan/add','form-laporan')->name('add-laporan');
+Volt::route('/laporan/{laporan}/edit','form-edit-laporan')->name('edit-laporan');
 
 Volt::route('/dashboard','dashboard');
 
