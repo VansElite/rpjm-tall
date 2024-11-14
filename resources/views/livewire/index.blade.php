f9550a038ed40be567fe6b6a8f5fe474f959c16d
Vendy Alifian <vanselite21@gmail.com>
2024-11-15 00:00:07 +0700

    rebuild the layout of sidebar on index, and try - error data kegiatan, which work surprisingly

--- resources/views/livewire/index.blade.php
+++ resources/views/livewire/index.blade.php
@@ -1,105 +1,80 @@
 <?php
 
-use App\Models\Bidang;
 use App\Models\Kegiatan;
-use App\Models\Laporan;
 use Livewire\Volt\Component;
+use Livewire\WithPagination;
 
 new class extends Component {
-    public Kegiatan $kegiatan;
-    Public Laporan $laporan;
-    public $kegiatans;
+    use WithPagination;
 
     public string $search;
 
     public $headers = [
-        ['key' => 'kegiatan.id', 'label' => 'No'],
-        ['key' => 'kegiatan.nama', 'label' => 'Nama Kegiatan'],
-        ['key' => 'kegiatan.dusun.nama', 'label' => 'Dusun'],
-        ['key' => 'progres', 'label' => 'Progres'],
+        ['key' => 'id', 'label' => 'No','class' => 'w-1'],
+        ['key' => 'nama', 'label' => 'Nama Kegiatan', 'class' => 'max-w-20'],
+        ['key' => 'dusun_nama', 'label' => 'Dusun', 'class' => 'max-w-10'],
+        ['key' => 'laporan_progres', 'label' => 'Progres', 'class' => 'max-w-10'],
     ];
 
-    public function mount()
-    {
-        $this->search = '';
 
+    public function render(): mixed
+    {
+        // $this->kegiatans = Laporan::with(['kegiatan','kegiatan.dusun'])->get();
+
+        return view('livewire.index', [
+            'kegiatans' => Kegiatan::withAggregate('laporan','progres')->withAggregate('dusun','nama')->paginate(5), // Menggunakan paginate
+        ]);
     }
 
-    public function mounted()
-    {
 
-    }
-
-    public function boot()
-    {
-        $this->kegiatans = Laporan::with(['kegiatan','kegiatan.dusun'])->get(); //Sementara pake laporan dulu
-
-    }
-
-    public function updatedSearch($namaKegiatan)
-    {
-        $this->kegiatans= Laporan::with(['kegiatan'=>function ($querry) {$querry->where('nama',$namaKegiatan);},'kegiatan.dusun'])->get();
-    }
+    // public function updatedSearch($namaKegiatan)
+    // {
+    //     $this->kegiatans= Laporan::with(['kegiatan'=>function ($querry) {$querry->where('nama',$namaKegiatan);},'kegiatan.dusun'])->get();
+    // }
 
 
 }; ?>
 
-<div class="grid h-full grid-cols-12">
-    <div class="grid h-full col-span-3 grid-rows-5">
+<div class="grid h-full grid-cols-12 gap-4">
+    <!-- Bagian Tabel (3 dari 12 kolom) -->
+    <div class="col-span-3 grid-rows-5">
+        <x-card class="row-span-1 text-sm">
+            searchbar
+        </x-card>
 
-        <div class="flex-auto row-span-1 gap-1 md:grid-row-2">
-            <input type="text" placeholder="ketik nama kegiatan" wire:model='search' class="w-4/5 row-span-1 mx-5">
-            <x-tabs wire:model="selectedTab" class="flex justify-center flex-auto row-span-1">
-                <x-tab name="users-tab" label="Users" icon="o-users">
-                    <div>Users</div>
-                </x-tab>
-                <x-tab name="tricks-tab" label="Tricks" icon="o-sparkles">
-                    <div>Tricks</div>
-                </x-tab>
-                <x-tab name="musics-tab" label="Musics" icon="o-musical-note">
-                    <div>Musics</div>
-                </x-tab>
-            </x-tabs>
-        </div>
-
-        <div class="row-span-3">
-            <x-table :headers="$headers" :rows="$kegiatans">
-                @scope('actions', $kegiatan)
-                <div class="flex gap-2">
-                    <x-button icon="o-folder-open" wire:click="#" spinner class="btn-sm" />
-                </div>
+        <x-card class="row-span-3 text-sm" title="Daftar Kegiatan">
+            <x-table :headers="$headers" :rows="$kegiatans" link="route('#')" class="w-full">
+                @scope('cell_progres', $kegiatan)
+                <x-badge class="badge-primary" />
                 @endscope
             </x-table>
-        </div>
-
-        <div class="grid grid-cols-3 row-span-1 gap-1">
-            <div class="flex-auto col-span-1">
-                <x-dropdown label="Filter 1" class="btn-outline" no-x-anchor>
-                    <x-menu-item title="Hey" />
-                    <x-menu-item title="How are you?" />
-                </x-dropdown>
-            </div>
-            <div class="flex-auto col-span-1">
-                <x-dropdown label="Filter 2" no-x-anchor>
-                    <x-menu-item title="Hey" />
-                    <x-menu-item title="How are you?" />
-                </x-dropdown>
-            </div>
-            <div class="flex-auto col-span-1">
-                <x-dropdown label="Filter 3" no-x-anchor>
-                    <x-menu-item title="Hey" />
-                    <x-menu-item title="How are you?" />
-                </x-dropdown>
-            </div>
-        </div>
-
-    </div>
-    {{-- @if ($showProgram) --}}
-    <div class="grid h-full col-span-4 md:grid-row-7">
-        <x-card title="Detail kegiatan X" class="row-span-4 mx-2 my-2 h-fit bg-base-200" separator>
 
         </x-card>
-        <x-card title="Laporan kegiatan X" class="row-span-3 mx-2 my-2 h-fit w-8/9 bg-base-200" separator>
+
+        <!-- Filter Dropdowns -->
+        <div class="flex row-span-1 gap-2 mt-4">
+            <x-dropdown label="Filter 1" class="btn-outline">
+                <x-menu-item title="Hey" />
+                <x-menu-item title="How are you?" />
+            </x-dropdown>
+            <x-dropdown label="Filter 2">
+                <x-menu-item title="Hey" />
+                <x-menu-item title="How are you?" />
+            </x-dropdown>
+            <x-dropdown label="Filter 3">
+                <x-menu-item title="Hey" />
+                <x-menu-item title="How are you?" />
+            </x-dropdown>
+        </div>
+    </div>
+
+    <!-- Bagian Card (4 dari 12 kolom) -->
+    <div class="col-span-4 space-y-4">
+        <x-card title="Detail kegiatan X" class="w-full h-80 bg-base-200" separator>
+            <!-- Content for detail can go here -->
+        </x-card>
+
+        <x-card title="Laporan kegiatan X" class="w-full h-80 bg-base-200" separator>
             <div>
                 List-item
             </div>
@@ -109,8 +84,8 @@ new class extends Component {
         </x-card>
     </div>
 
-    <div class="w-full h-full col-span-5" id='peta'></div>
-    {{-- @endif --}}
+    <!-- Bagian Map (5 dari 12 kolom) -->
+    <div class="w-full h-full col-span-5" id="peta"></div>
 </div>
 
 @script
