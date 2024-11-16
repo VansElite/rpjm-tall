171c8804d136b73fda4743cae8b0c2b2b867c4db
Vendy Alifian <vanselite21@gmail.com>
2024-11-17 00:33:02 +0700

    add js for create Marker and it change every page's <idk how to describe it actually its yea just like that>

--- resources/views/livewire/index.blade.php
+++ resources/views/livewire/index.blade.php
@@ -22,8 +22,7 @@ on.showDetail (kegiatan) {
 
 ";
 
-new class extends Component
-{
+new class extends Component {
     use WithPagination;
 
     public string $search;
@@ -34,27 +33,22 @@ new class extends Component
 
     public $detailKegiatan = [];
 
+    public $dataMap = [];
+
     public $headers = [
         ['key' => 'nama', 'label' => 'Nama Kegiatan', 'class' => 'text-center w-1/4'],
         ['key' => 'dusun_nama', 'label' => 'Dusun', 'class' => 'text-center w-1/6'],
         ['key' => 'latest_progres', 'label' => 'Progres', 'class' => 'text-center w-1/6'],
     ];
 
-    // public $coordinates;
-
-    public function mount()
-    {
-        // $this->coordinates = Kegiatan::select('id', 'latitude', 'longitude')->get();
-        // dd($this->coordinates[0]['latitude']);
-    }
 
     public function render(): mixed
     {
         // $this->kegiatans = Laporan::with(['kegiatan','kegiatan.dusun'])->get();
 
-        $kegiatans = Kegiatan::withAggregate('dusun', 'nama')->withAggregate('laporan', 'progres')
-            ->with('latestProgress')
-            ->paginate(5);
+        $kegiatans = Kegiatan::withAggregate('dusun', 'nama')->withAggregate('laporan', 'progres')->with('latestProgress', 'program.bidang')->paginate(5);
+
+        $this->setupDataMaps($kegiatans);
 
         // Menambahkan progres terakhir ke dalam array headers untuk setiap kegiatan
         foreach ($kegiatans as $kegiatan) {
@@ -69,14 +63,31 @@ new class extends Component
     // Fungsi untuk memilih kegiatan dan menampilkan detailnya
     public function selectKegiatan($id)
     {
-        $this->selectedKegiatan = Kegiatan::with(['dusun', 'laporan'])->find($id);
+        $this->selectedKegiatan = Kegiatan::with(['dusun', 'laporan', 'program.bidang'])->find($id);
         $this->setupDetailKegiatan();
-        $this->showDetail = ! $this->showDetail;
+
+        $this->showDetail = !$this->showDetail;
     }
+
+    public function setupDataMaps($data){
+
+        $this->dataMap = $data
+            ->map(function ($kegiatan) {
+                return [
+                    'id' => $kegiatan->id,
+                    'nama' => $kegiatan->nama,
+                    'longitude' => $kegiatan->longitude,
+                    'latitude' => $kegiatan->latitude,
+                    'bidang_nama' => $kegiatan->program->bidang->nama ?? 'Tidak Ada Bidang',
+                ];
+            })
+            ->toArray();
+    }
+
 
     public function getShowDetailProperty()
     {
-        return ! is_null($this->selectedKegiatan);
+        return !is_null($this->selectedKegiatan);
     }
 
     public function setupDetailKegiatan()
@@ -88,9 +99,7 @@ new class extends Component
             ],
             [
                 'name' => 'Progress',
-                'data' => $this->selectedKegiatan->laporan->isNotEmpty()
-                    ? $this->selectedKegiatan->laporan->last()->progres.'%'
-                    : '0%',
+                'data' => $this->selectedKegiatan->laporan->isNotEmpty() ? $this->selectedKegiatan->laporan->last()->progres . '%' : '0%',
             ],
             [
                 'name' => 'Bidang',
@@ -106,16 +115,11 @@ new class extends Component
             ],
             [
                 'name' => 'Volume',
-                'data' => $this->selectedKegiatan->volume.' '.$this->selectedKegiatan->satuan,
+                'data' => $this->selectedKegiatan->volume . ' ' . $this->selectedKegiatan->satuan,
             ],
             [
                 'name' => 'Tahun',
-                'data' => ($this->selectedKegiatan->tahun_1 ? '1, ' : '')
-                    .($this->selectedKegiatan->tahun_2 ? '2, ' : '')
-                    .($this->selectedKegiatan->tahun_3 ? '3, ' : '')
-                    .($this->selectedKegiatan->tahun_4 ? '4, ' : '')
-                    .($this->selectedKegiatan->tahun_5 ? '5, ' : '')
-                    .($this->selectedKegiatan->tahun_6 ? '6' : ''),
+                'data' => ($this->selectedKegiatan->tahun_1 ? '1, ' : '') . ($this->selectedKegiatan->tahun_2 ? '2, ' : '') . ($this->selectedKegiatan->tahun_3 ? '3, ' : '') . ($this->selectedKegiatan->tahun_4 ? '4, ' : '') . ($this->selectedKegiatan->tahun_5 ? '5, ' : '') . ($this->selectedKegiatan->tahun_6 ? '6' : ''),
             ],
             [
                 'name' => 'Lokasi',
@@ -123,7 +127,7 @@ new class extends Component
             ],
             [
                 'name' => 'Koordinat',
-                'data' => $this->selectedKegiatan->latitude.', '.$this->selectedKegiatan->longitude,
+                'data' => $this->selectedKegiatan->latitude . ', ' . $this->selectedKegiatan->longitude,
             ],
             [
                 'name' => 'Deskripsi',
@@ -147,134 +151,115 @@ new class extends Component
                 </tr>
             </thead>
             <tbody>
-                @foreach($kegiatans as $row)
-                <tr class="hover">
-                    <th>{{ $loop->index + $kegiatans->firstItem() }}</th>
-                    <td>{{ $row->nama }}</td>
-                    <td>{{ $row->dusun->nama }}</td>
-                    <td>{{ $row->latest_progres }}%</td>
-                    <th>
-                        <button
-                            class="btn btn-ghost btn-xs"
-                            wire:click="selectKegiatan({{ $row->id }})"
-                        >
-                            details
-                        </button>
-                    </th>
-                </tr>
+                @foreach ($kegiatans as $row)
+                    <tr class="hover">
+                        <th>{{ $loop->index + $kegiatans->firstItem() }}</th>
+                        <td>{{ $row->nama }}</td>
+                        <td>{{ $row->dusun->nama }}</td>
+                        <td>{{ $row->latest_progres }}%</td>
+                        <th>
+                            <button class="btn btn-ghost btn-xs" wire:click="selectKegiatan({{ $row->id }})">
+                                details
+                            </button>
+                        </th>
+                    </tr>
                 @endforeach
             </tbody>
         </table>
 
         <div class="flex items-center">
-            <p class="text-sm flex-grow opacity-80">
-                <b>{{ $kegiatans->firstItem() }} - {{ $kegiatans->lastItem() }}</b> dari <b>{{ $kegiatans->total() }}</b>
+            <p class="flex-grow text-sm opacity-80">
+                <b>{{ $kegiatans->firstItem() }} - {{ $kegiatans->lastItem() }}</b> dari
+                <b>{{ $kegiatans->total() }}</b>
             </p>
             <div class="join">
-                <button
-                    class="join-item btn btn-xs"
-                    wire:click="gotoPage(1, 'page')"
-                ><<</button>
-                <button
-                    class="join-item btn btn-xs"
-                    wire:click="previousPage('page')"
-                ><</button>
+                <button class="join-item btn btn-xs" wire:click="gotoPage(1, 'page')"> << </button>
+                <button class="join-item btn btn-xs" wire:click="previousPage('page')"> < </button>
                 <button class="join-item btn btn-xs">Page {{ $kegiatans->currentPage() }}</button>
-                <button
-                    class="join-item btn btn-xs"
-                    wire:click="nextPage('page')"
-                >></button>
-                <button
-                    class="join-item btn btn-xs"
-                    wire:click="gotoPage({{ $kegiatans->lastPage() }}, 'page')"
-                >>></button>
+                <button class="join-item btn btn-xs" wire:click="nextPage('page')"> > </button>
+                <button class="join-item btn btn-xs" wire:click="gotoPage({{ $kegiatans->lastPage() }}, 'page')">>></button>
             </div>
         </div>
     </div>
 
     <!-- Bagian Card (4 dari 12 kolom), ditampilkan berdasarkan showDetail -->
     @if ($showDetail)
-    <div class="p-4 bg-base-200 overflow-auto">
-        <!-- Detail Kegiatan -->
-        <div class="mb-4 flex items-center">
-            <h2 class="text-md font-bold">Detail Kegiatan {{ $selectedKegiatan->nama }}</h2>
-            <x-button
-                icon="m-pencil-square"
-                link="{{ route('edit-kegiatan', ['kegiatan' => $selectedKegiatan->id]) }}"
-                spinner
-                class="ml-2 btn-sm btn-ghost"
-            />
-        </div>
+        <div class="p-4 overflow-auto bg-base-200">
+            <!-- Detail Kegiatan -->
+            <div class="flex items-center mb-4">
+                <h2 class="font-bold text-md">Detail Kegiatan {{ $selectedKegiatan->nama }}</h2>
+                <x-button icon="m-pencil-square"
+                    link="{{ route('edit-kegiatan', ['kegiatan' => $selectedKegiatan->id]) }}" spinner
+                    class="ml-2 btn-sm btn-ghost" />
+            </div>
 
-        <div class="flex flex-col">
-            <table class="table">
-                <tbody>
-                    @foreach($detailKegiatan as $d)
-                    <tr>
-                        <td class="opacity-60 p-0">{{ $d['name'] }}</td>
-                        <td class="p-0">: {{ $d['data'] }}</td>
-                    </tr>
-                    @endforeach
-                </tbody>
-            </table>
-        </div>
+            <div class="flex flex-col">
+                <table class="table">
+                    <tbody>
+                        @foreach ($detailKegiatan as $d)
+                            <tr>
+                                <td class="p-0 opacity-60">{{ $d['name'] }}</td>
+                                <td class="p-0">: {{ $d['data'] }}</td>
+                            </tr>
+                        @endforeach
+                    </tbody>
+                </table>
+            </div>
 
-        <!-- Laporan Kegiatan -->
-        <div class="flex my-4">
-            <h2 class="text-md font-bold">Laporan Kegiatan</h2>
-            <x-button
-                icon="o-plus"
-                link="{{
-                    route('add-laporan', [
+            <!-- Laporan Kegiatan -->
+            <div class="flex my-4">
+                <h2 class="font-bold text-md">Laporan Kegiatan</h2>
+                <x-button icon="o-plus"
+                    link="{{ route('add-laporan', [
                         'selectedBidang' => $selectedKegiatan->program->bidang->id,
                         'selectedProgram' => $selectedKegiatan->program->id,
                         'selectedKegiatan' => $selectedKegiatan->id,
-                    ])
-                }}"
-                class="col-span-1 ml-2 justify-content-center h-fit btn-square btn-xs btn-outline"
-            />
-        </div>
-        <ul class="timeline timeline-vertical">
-            @foreach ($selectedKegiatan->laporan as $laporan)
-                <li>
-                    <hr />
-                    <div class="timeline-start text-right">
-                        {{ $laporan->progres }}%
-                        <p class="opacity-80">{{ $laporan->created_at->format('d-M-Y H:i') }}</p>
-                    </div>
-                    <div class="timeline-middle">
-                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="h-5 w-5">
-                            <path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z" clip-rule="evenodd" />
-                        </svg>
-                    </div>
-                    <div class="timeline-end">
-                        <div class="flex items-center">
-                            <span class="font-bold">{{ $laporan->judul}}</span>
-                            <x-button
-                                icon="m-pencil-square"
-                                link="{{ route('edit-laporan', ['laporan' => $laporan->id]) }}"
-                                spinner
-                                class="ml-2 btn-sm btn-ghost"
-                            />
+                    ]) }}"
+                    class="col-span-1 ml-2 justify-content-center h-fit btn-square btn-xs btn-outline" />
+            </div>
+            <ul class="timeline timeline-vertical">
+                @foreach ($selectedKegiatan->laporan as $laporan)
+                    <li>
+                        <hr />
+                        <div class="text-right timeline-start">
+                            {{ $laporan->progres }}%
+                            <p class="opacity-80">{{ $laporan->created_at->format('d-M-Y H:i') }}</p>
                         </div>
-                        <p class="opacity-80">{{ $laporan->deskripsi}}</p>
-                    </div>
-                    <hr />
-                </li>
-            @endforeach
-        </ul>
-    </div>
+                        <div class="timeline-middle">
+                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor"
+                                class="w-5 h-5">
+                                <path fill-rule="evenodd"
+                                    d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z"
+                                    clip-rule="evenodd" />
+                            </svg>
+                        </div>
+                        <div class="timeline-end">
+                            <div class="flex items-center">
+                                <span class="font-bold">{{ $laporan->judul }}</span>
+                                <x-button icon="m-pencil-square"
+                                    link="{{ route('edit-laporan', ['laporan' => $laporan->id]) }}" spinner
+                                    class="ml-2 btn-sm btn-ghost" />
+                            </div>
+                            <p class="opacity-80">{{ $laporan->deskripsi }}</p>
+                        </div>
+                        <hr />
+                    </li>
+                @endforeach
+            </ul>
+        </div>
     @endif
 
     <!-- Bagian Map (5 dari 12 kolom) -->
-    <div class="{{ $showDetail ? 'col-span-1' : 'col-span-2' }}">
+    <div class="{{ $showDetail ? 'col-span-1' : 'col-span-2' }}" did="peta-wrap">
         <div wire:ignore class="w-full h-full" id="peta"></div>
     </div>
 </div>
 
 @script
     <script>
-        mapboxgl.accessToken = 'pk.eyJ1IjoidmFuc2VsaXRlMjEiLCJhIjoiY20yeWd2dDZyMDB3MjJtc2piZjE1ZDk0OSJ9.yDmaTMSvuPWK-iDhvldKWg';
+        mapboxgl.accessToken =
+            'pk.eyJ1IjoidmFuc2VsaXRlMjEiLCJhIjoiY20yeWd2dDZyMDB3MjJtc2piZjE1ZDk0OSJ9.yDmaTMSvuPWK-iDhvldKWg';
+
         const map = new mapboxgl.Map({
             container: 'peta', // container ID
             style: 'mapbox://styles/mapbox/streets-v12', // style URL
@@ -282,6 +267,50 @@ new class extends Component
             zoom: 13, // starting zoom
         });
 
+        const warnaBidang = {
+            'Pendidikan': '#40e0d0',
+            'Kesehatan': '#b4c6d0',
+            'Penataan Ruang': '#4a8bad',
+            'Kawasan Pemukiman': '#ffff00',
+            'Lingkungan Hidup': '#00ff00',
+            'Perhubungan dan Infokom': '#e23a08',
+            'Pariwisata': '#492971',
+            'Default': '#42f545',
+        };
 
+        const createMarker = (coordinate, map, color, title) => {
+            return new mapboxgl.Marker({
+                    color: color
+                })
+                .setPopup(new mapboxgl.Popup().setText(title))
+                .setLngLat(coordinate)
+                .addTo(map);
+        };
+
+        let kegiatanMarkers = [];
+        function renderMarkers(dataMap, map) {
+            if (kegiatanMarkers.length > 0) {
+                kegiatanMarkers.forEach(kegiatanMarker => kegiatanMarker.remove())
+            }
+            kegiatanMarkers = dataMap.map(kegiatan => createMarker(
+                [kegiatan.longitude, kegiatan.latitude],
+                map,
+                warnaBidang[kegiatan.bidang_nama] || warnaBidang['Default'],
+                kegiatan.nama
+            ));
+            window.kegiatanMarkers = kegiatanMarkers;
+        };
+
+        Livewire.hook('morph.updated', ({ el, component }) => {
+            if(el.getAttribute('did') == 'peta-wrap') {
+                window.kegiatanMarkers.map(r => r.remove())
+                let dataMap = $wire.get('dataMap');
+                renderMarkers(dataMap, map);
+            }
+        });
+
+
+        let dataMap = $wire.get('dataMap');
+        renderMarkers(dataMap, map);
     </script>
 @endscript
